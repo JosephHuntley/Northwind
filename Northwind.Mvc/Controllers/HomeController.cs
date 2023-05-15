9016f131ea817d025dff72b8e42d2516f3998f83
Joseph Huntley <Joseph.Huntley@outlook.com>
2023-05-15 09:13:24 -0400

    Init webapi

---
+++ Northwind.Mvc/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Northwind.Mvc.Models;
+
+namespace Northwind.Mvc.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        // View looks in different paths for the Razor file to generate the web page. The file name must be the same as the method.
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ Northwind.Mvc/Program.cs
@@ -0,0 +1,52 @@
+// Section 1 - Import Section (Namespace)
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Northwind.Mvc.Data;
+
+// Section 2 - configure the host web server including services
+// Dependency Injection 
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Get connection string, and inject dbContext for authentication 
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlite(connectionString));
+
+// Error page during development
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+
+// Section 3 - Configure HTTTP Pipeline
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseMigrationsEndPoint();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapRazorPages();
+
+// Section 4 - start the host web server listening for HTTP requests
+app.Run();
